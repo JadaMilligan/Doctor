a4aed5e74d8c88630253d549ce349ef8f1bd48ad
JadaMilligan <70172897+JadaMilligan@users.noreply.github.com>
2020-08-31 16:58:21 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String patientName= "Stacey Manson";
+        String hasInsurance= "false";
+        double temp=102.2;
+        int numVisits=5;
+        float coPay= 130.50f;
+        String nextAppt= "Sept. 3rd";
+        String contact= "404-955-3985";
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: "+patientName);
+        System.out.println("Patient has insurance: "+hasInsurance);
+        System.out.println("Body temperature: " + temp);
+        System.out.println("Number of visits:" + numVisits);
+
+    }
+}
